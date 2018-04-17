f2770c856f8273e06521dcde7057a5ee8af2d80d
cheitovilla <cheitovilla@outlook.com>
2018-04-16 20:07:58 -0500

    Hidrante hecho

---
+++ ProjectZombie/Assets/SceneHuidranteALL/Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour {
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Desarmar(){
+        anim.SetTrigger("desarma");
+    }
+
+    public void Volver() {
+        anim.SetTrigger("idle");
+    }
+}
