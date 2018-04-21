a1e12f05951edd51f8deac28aebed9a8b95311db
cheitovilla <cheitovilla@outlook.com>
2018-04-20 20:23:23 -0500

    ready

--- ProjectZombie/Assets/ESTO ES LO DE LA CLASE/Script/Manager.cs
+++ ProjectZombie/Assets/ESTO ES LO DE LA CLASE/Script/Manager.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour {
 
-   
 
     // Use this for initialization
     void Start () {
         string[] names = new string[] {
-            "stubbs",
-            "rob",
-            "white"
+            "Stubbs", "Rob", "White", "Yasuli yamile", "Mohame Ali", "Mohame Aca", "Cuchin", "Clara", "Bladimir", "Trump", "Santos", "Marquez", "Mc Flurry", "Carlos", "Fredi", "Juanda", "Pato", "Mia",
+            "Coluchi", "Elkin"
         };
 
-
-
+       
         int randon_zombies = Random.Range(5, 10);
         for (int i = 0; i < randon_zombies; i++)
         {
@@ -23,7 +20,17 @@ public class Manager : MonoBehaviour {
             Debug.Log("Soy un zombie color " + z.Name_color);
         }
 
-        
+        for (int j = 0; j < names.Length; j++)
+        {
+            int rango_edad = Random.Range(15, 100);
+            Zombie z = new Zombie(1);
+            Debug.Log("Hola soy " + names[j] + ", y mi edad es " + rango_edad);
+        }
+
+        for (int k = 0; k < 1; k++)
+        {
+            Zombie z = new Zombie(2);
+        }
 
     }
 
@@ -32,10 +39,5 @@ public class Manager : MonoBehaviour {
 		
 	}
 
-    public void Heroe() {
-       
-    }
-
-    
 
 }
--- ProjectZombie/Assets/ESTO ES LO DE LA CLASE/Script/Zombie.cs
+++ ProjectZombie/Assets/ESTO ES LO DE LA CLASE/Script/Zombie.cs
@@ -6,15 +6,36 @@ public class Zombie  {
 
   
     GameObject zombieMesh;
-    string colorito;
+    string name_col;
 
     public Zombie(int clase) {
-      
-        zombieMesh = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        zombieMesh.GetComponent<Renderer>().material.color = Colores();
-        Vector3 pos = new Vector3(Random.Range(-10,10), 0, Random.Range(-10,10));
-        zombieMesh.transform.position = pos;
-        colorito = zombieMesh.GetComponent<Renderer>().material.color.ToString();
+
+        if (clase == 0) // zombies
+        {
+            zombieMesh = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            zombieMesh.GetComponent<Renderer>().material.color = Colores();
+            Vector3 pos = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+            zombieMesh.transform.position = pos;
+            name_col = zombieMesh.GetComponent<Renderer>().material.color.ToString();
+        }
+        if (clase == 1) //Humanos
+        {
+            zombieMesh = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            zombieMesh.GetComponent<Renderer>().material.color = Color.white;
+            Vector3 pos = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+            zombieMesh.transform.position = pos;
+            name_col = zombieMesh.GetComponent<Renderer>().material.color.ToString();
+        }
+        if (clase ==2)
+        {
+            zombieMesh = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+            zombieMesh.GetComponent<Renderer>().material.color = Color.black;
+            Vector3 pos = new Vector3(0, 0, 0);
+            zombieMesh.transform.position = pos;
+            zombieMesh.AddComponent<FPSMove>();
+            zombieMesh.AddComponent<FPSAim>();
+            zombieMesh.AddComponent<Camera>();
+        }
     }
 	
 	// Update is called once per frame
@@ -43,10 +64,10 @@ public class Zombie  {
     public string Name_color
     {
         set{
-            colorito = value;
+            name_col = value;
         }
         get{
-            return colorito;
+            return name_col;
         }
     }
 
