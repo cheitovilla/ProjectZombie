75b45ad8f6624c52fe13e14944098f0a12874e22
cheitovilla <cheitovilla@outlook.com>
2018-04-24 18:13:33 -0500

    Ready player one

--- ProjectZombie/Assets/ESTO ES LO DE LA CLASE/Script/Manager.cs
+++ ProjectZombie/Assets/ESTO ES LO DE LA CLASE/Script/Manager.cs
@@ -15,29 +15,24 @@ public class Manager : MonoBehaviour {
         };
 
        //Aqui se llaman a los zombies
-        int randon_ = Random.Range(5,10); 
-        for (int i = 0; i < randon_; i++)
-        {
-            Zombie z = new Zombie();
-            Debug.Log("Soy un zombie color " + z.Name_color);
-
-            int rango_edad = Random.Range(15, 100);
-            Ciudadanos c = new Ciudadanos();
-            Debug.Log("Hola soy " + names[i] + ", y mi edad es " + rango_edad);
-        }
-
-        //Aqui se crean los ciudadanos
-        //for (int j = 0; j < randon_; j++) 
-        //{
-        //    int rango_edad = Random.Range(15, 100);
-        //    Ciudadanos z = new Ciudadanos();
-        //    Debug.Log("Hola soy " + names[j] + ", y mi edad es " + rango_edad);
-        //}
-
-        //Aquí se llama al heroe
+        int randon_ = Random.Range(5,10);
         Hero h = new Hero();
-    
-
+        for (int i = 1; i < randon_; i++)
+        {
+            int ale = Random.Range(1, 3);
+     
+            if (ale ==1)
+            {
+                Zombie z = new Zombie();
+                Debug.Log("Soy un zombie color " + z.Name_color);
+            }
+           else
+            {
+                int rango_edad = Random.Range(15, 100);
+                Ciudadanos c = new Ciudadanos();
+                Debug.Log("Hola soy " + names[i] + ", y mi edad es " + rango_edad);
+            }   
+        }
     }
 
 }
