fe79586fb045e91aed4820c793049ea5f440ad2a
cheitovilla <cheitovilla@outlook.com>
2018-04-24 20:47:09 -0500

    empezando hacer la guia 2

---
+++ ProjectZombie/Assets/ESTO ES LO DE LA CLASE/Script/Monster.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public enum MonsterState
+    {
+        standing,
+        wandering,
+        chasing,
+        attacking
+    }
+
+    public MonsterState mState;
+
+    void Start()
+    {
+        mState = MonsterState.attacking;
+        int number = (int)mState;
+        Debug.Log(number);
+
+    }
+   
+  
+
+
+	void Update ()
+	{
+		
+	}
+}
---
+++ ProjectZombie/Assets/ESTO ES LO DE LA CLASE/Script/MonsterSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour 
+{
+
+    public PrimitiveType primitiveType;
+    GameObject obj;
+
+
+	void Start () 
+	{
+        obj = GameObject.CreatePrimitive(primitiveType);
+
+        if (myColor == colorType.red)
+        {
+            obj.GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (myColor == colorType.blue)
+        {
+            obj.GetComponent<Renderer>().material.color = Color.blue;
+        }
+        if (myColor == colorType.green)
+        {
+            obj.GetComponent<Renderer>().material.color = Color.green;
+        }
+
+
+	}
+
+	void Update () 
+	{
+
+	}
+
+    public enum colorType {
+        red,blue,green
+    }
+
+    public colorType myColor;
+
+}
